0201a0b8bf55ca5ddba7c9104e6d0abecf5f3f91
Tom Barone <mail@tombarone.net>
2021-01-11 17:06:24 +1030

    Add cocoon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,4 @@
 module ApplicationHelper
-  def button_to_add_row(f, association, **args)
-    new_object = f.object.send(association).klass.new
-    id = new_object.object_id
-    fields =
-      f.simple_fields_for(
-        association,
-        new_object,
-        child_index: id
-      ) { |builder| render(association.to_s.singularize, f: builder) }
-
-    "<a href='#' class='#{('add_fields' + ' ' + args[:class]).html_safe}' data-id='#{id}' data-fields='#{fields.gsub("\n", '')}'>
-      <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-plus-circle-fill' viewBox='0 0 16 16'>
-        <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z'/>
-      </svg>
-    </a>".html_safe
-  end
-
   class BootstrapSmallFormBuilder < ActionView::Helpers::FormBuilder
     def collection_select(
       method,
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 require("jquery");
+require("@nathanvda/cocoon");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,4 +17,5 @@ require("jquery");
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 //
-//
+require("./applications_edit_page.js");
+require("./applications_index_page.js");
