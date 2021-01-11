db074883117545ad71071c0a1316cf272986ae52
Tom Barone <mail@tombarone.net>
2021-01-12 09:04:20 +1030

    test

--- app/models/application.rb
+++ app/models/application.rb
@@ -30,83 +30,6 @@ class Application < ApplicationRecord
   CONSENT = %w[Approved Refused]
   CERTIFIER = %w[Vic Peter]
 
-  scope :filter_all,
-        ->(params) {
-          filter_by_search_text(params[:search_text])
-            .filter_by_type(params[:type])
-            .filter_by_date(params[:start_date], params[:end_date])
-            # Show PC's first, then Q's
-            .order(
-              'field (application_type_id, 3, 4, 1, 5, 2, 6) asc, reference_number desc'
-            )
-        }
-
-  scope :filter_by_type,
-        ->(application_type) {
-          if application_type != nil
-            joins(:application_type).where(
-              "application_types.application_type = '#{application_type}'" # Default to PC's
-            )
-          else
-            nil
-          end
-        }
-
-  scope :filter_by_date,
-        ->(start_date, end_date) {
-          if start_date != nil or end_date != nil
-            where(
-              "applications.created_at >= '#{
-                start_date ||= '1900-01-01'
-              }' and applications.created_at <= '#{end_date ||= DateTime.now}'"
-            )
-          else
-            nil
-          end
-        }
-
-  scope :filter_by_search_text,
-        ->(search_text) {
-          if search_text != nil
-            joins('left join suburbs s on applications.suburb_id = s.id')
-              .joins(
-                'left join clients contact on applications.client_id = contact.id'
-              )
-              .joins(
-                'left join clients owner on applications.owner_id = owner.id'
-              )
-              .joins(
-                'left join clients applicant on applications.applicant_id = applicant.id'
-              )
-              .joins('left join councils c on applications.council_id = c.id')
-              .where(
-                'match(
-                  development_application_number,
-                  street_name,
-                  street_number,
-                  lot_number,
-                  description
-              ) against (? in boolean mode)
-              or match(s.display_name) against (? in boolean mode) 
-              or match(contact.client_name) against (? in boolean mode) 
-              or match(owner.client_name) against (? in boolean mode) 
-              or match(applicant.client_name) against (? in boolean mode) 
-              or match(c.name) against (? in boolean mode) 
-              or reference_number like ?
-              ',
-                search_text,
-                search_text,
-                search_text,
-                search_text,
-                search_text,
-                search_text,
-                '%' + search_text + '%'
-              )
-          else
-            nil
-          end
-        }
-
   def suburb_display_name=(display_name)
     self.suburb = Suburb.find_by(display_name: display_name)
   end
@@ -139,4 +62,24 @@ class Application < ApplicationRecord
   def client_name
     self.client ? self.client.client_name : nil
   end
+
+  def applicant_council_name=(name)
+    self.applicant_council = Council.find_or_create_by(name: name)
+  end
+  def applicant_council_name
+    self.applicant_council ? self.applicant_council.name : nil
+  end
+  def owner_council_name=(name)
+    self.owner_council = Council.find_or_create_by(name: name)
+  end
+  def owner_council_name
+    self.owner_council ? self.owner_council.name : nil
+  end
+  def client_council_name=(name)
+    self.client_council = Council.find_or_create_by(name: name)
+  end
+  def client_council_name
+    self.client_council ? self.client_council.name : nil
+  end
+
 end
