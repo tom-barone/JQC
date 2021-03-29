99faf79227c2e513a578bb3f12d21c2bdfdefaf4
Tom Barone <mail@tombarone.net>
2021-03-29 21:14:42 +1030

    Try updating rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_093710) do
+ActiveRecord::Schema.define(version: 2021_03_29_104056) do
 
   create_table "application_additional_informations", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "info_date"
