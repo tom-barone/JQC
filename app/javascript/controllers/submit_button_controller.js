94cc87e95cf5e88f9bfbbf508398b18990f54919
Tom Barone <mail@tombarone.net>
2022-11-17 00:26:56 +0100

    Rename submit button javascript and fix system test

--- app/javascript/controllers/submit_button_controller.js
+++ app/javascript/controllers/submit_button_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  setSpinner(event) {
+  onSubmit(event) {
     this.element.innerHTML = `<span class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span> ${
       event.params.loadingText || ""
     }`;
--- test/system/applications_basic_test.rb
+++ test/system/applications_basic_test.rb
@@ -3,39 +3,48 @@
 require 'application_system_test_case'
 
 class ApplicationsTest < ApplicationSystemTestCase
-
   test 'The main application page' do
     sign_in_test_user
 
-    # Ask the browser what it thinks todays date is (including timezone)
-    current_javascript_time = evaluate_script('new Date().toISOString()')
-    current_ruby_time = Time.zone.parse(current_javascript_time)
-    current_date_string = current_ruby_time.strftime('%Y-%m-%d')
+    yesterday = (Time.zone.now - 1.day).strftime('%Y-%m-%d')
+    today = Time.zone.now.strftime('%Y-%m-%d')
+    tomorrow = (Time.zone.now + 1.day).strftime('%Y-%m-%d')
 
-    # Check the date fields are setup correctly so that you can't search in the future
-    assert_selector("#start-datepicker[max=\"#{current_date_string}\"]")
-    assert_selector("#end-datepicker[max=\"#{current_date_string}\"]")
+    # Check the search datepickers are setup correctly so you can't search in the future
+    # NOTE: Accept a range of 3 days, because it's too hard to try matching timezones between 
+    # the headless browser and ruby, and could even fail anyway if the browser loads 
+    # at 11:59:59pm and this test runs at 12:00:01am
+    assert_any_of_selectors(
+      "#start-datepicker[max=\"#{yesterday}\"]",
+      "#start-datepicker[max=\"#{today}\"]",
+      "#start-datepicker[max=\"#{tomorrow}\"]"
+    )
+    assert_any_of_selectors(
+      "#end-datepicker[max=\"#{yesterday}\"]",
+      "#end-datepicker[max=\"#{today}\"]",
+      "#end-datepicker[max=\"#{tomorrow}\"]"
+    )
 
     #created_pcs = create_list(:application, 5)
     ##created_lgs =
-      ##create_list(:application, 5) do |application, i|
-        ##application.reference_number = "LG#{i}"
-      ##end
+    ##create_list(:application, 5) do |application, i|
+    ##application.reference_number = "LG#{i}"
+    ##end
 
     #sign_in_test_user
 
     ## Test both urls "/" and "/applications" show the applications table
     #[root_path, applications_url].each do |url|
-      #visit url
-      #assert_text 'Reference Number'
+    #visit url
+    #assert_text 'Reference Number'
 
-      ## Check all the PC & LG reference numbers are there
-      #created_pcs.each do |application|
-        #assert_text application.reference_number
-      #end
-      ##created_lgs.each do |application|
-        ##assert_text application.reference_number
-      ##end
+    ## Check all the PC & LG reference numbers are there
+    #created_pcs.each do |application|
+    #assert_text application.reference_number
+    #end
+    ##created_lgs.each do |application|
+    ##assert_text application.reference_number
+    ##end
     #end
 
     # Test that all column headers are shown
