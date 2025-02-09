7da94549b7ef27ae4301eb951e766cf59b938fce
Tom Barone <mail@tombarone.net>
2025-02-09 22:48:51 +1030

    Fix simple form issue

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -175,3 +175,38 @@ SimpleForm.setup do |config|
   # config.input_field_valid_class = 'is-valid'
   # config.input_field_error_class = 'is-invalid'
 end
+
+SimpleForm.setup do |config|
+  config.wrappers :sign_in_input,
+                  tag: 'div',
+                  class: 'form-group',
+                  error_class: 'form-group-invalid',
+                  valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.optional :maxlength
+    b.optional :minlength
+    b.optional :pattern
+    b.optional :min_max
+    b.optional :readonly
+    b.use :label
+    b.use :input,
+          class: 'form-control',
+          error_class: 'is-invalid',
+          valid_class: ''
+    b.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+  end
+
+  config.wrappers :sign_in_checkbox, tag: 'fieldset', class: 'form-group', error_class: 'form-group-invalid',
+                                     valid_class: 'form-group-valid' do |b|
+    b.use :html5
+    b.optional :readonly
+    b.wrapper :form_check_wrapper, tag: 'div', class: 'form-check' do |bb|
+      bb.use :input, class: 'form-check-input', error_class: 'is-invalid', valid_class: ''
+      bb.use :label, class: 'form-check-label'
+      bb.use :full_error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+      bb.use :hint, wrap_with: { tag: 'small', class: 'form-text text-muted' }
+    end
+  end
+end
