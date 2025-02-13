3d7a307c0a88ddebe20ec00a102d094fcbdc1394
Tom Barone <mail@tombarone.net>
2025-02-13 23:54:44 +1030

    Add correct icons and spruce up sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  authenticated :user do
+    # Define authenticated routes here
+    root to: 'home#index', as: :authenticated_root
+  end
+
   # Defines the root path route ("/")
-  root to: 'home#index'
+  root to: redirect('/users/sign_in')
 end
