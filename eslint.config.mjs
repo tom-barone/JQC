2d7ce1db767274ad437611ab180634ccb96149e9
Tom Barone <mail@tombarone.net>
2025-02-15 21:33:04 +1030

    Fix eslint jest config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,22 @@ import pluginJest from "eslint-plugin-jest";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
-  pluginJest.configs.recommended,
+  {
+    files: ["**/*.spec.js", "**/*.test.js"],
+    plugins: { jest: pluginJest },
+    languageOptions: {
+      globals: pluginJest.environments.globals.globals,
+    },
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+    },
+  },
 ];
