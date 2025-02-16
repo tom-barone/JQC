81b1734770929b094472677c87793b6ed49dca13
Tom Barone <mail@tombarone.net>
2025-02-16 22:56:50 +1030

    Update export to spreadsheet options

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
 class Application < ApplicationRecord
   belongs_to :suburb, optional: true
   belongs_to :council, optional: true
@@ -84,16 +85,66 @@ class Application < ApplicationRecord
   }
 
   def self.search(params)
-    Application.eager_load(:suburb, :council, :applicant, :application_type)
+    Application.eager_load(:application_type, :applicant, :owner, :contact, :council, :suburb)
                .filter_by_type(params[:type])
                .filter_by_date(params[:start_date], params[:end_date])
                .filter_by_search_text(params[:search_text])
                .order_by_type_and_reference_number
   end
 
-  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
   def self.write_csv_response(applications, response)
-    attributes = %w[reference_number description]
+    # Eager load the associations we need to export
+    applications = applications.includes(:request_for_informations).includes(:invoices)
+
+    columns = [
+      ['id', ->(app) { app.id }],
+      ['application_type', ->(app) { app.application_type&.application_type }],
+      ['reference_number', ->(app) { app.reference_number }],
+      ['converted_to_from', ->(app) { app.converted_to_from }],
+      ['council', ->(app) { app.council&.name }],
+      ['development_application_number', ->(app) { app.development_application_number }],
+      ['applicant', ->(app) { app.applicant&.client_name }],
+      ['owner', ->(app) { app.owner&.client_name }],
+      ['contact', ->(app) { app.contact&.client_name }],
+      ['description', ->(app) { app.description }],
+      ['cancelled', ->(app) { app.cancelled }],
+      ['street_number', ->(app) { app.street_number }],
+      ['lot_number', ->(app) { app.lot_number }],
+      ['street_name', ->(app) { app.street_name }],
+      ['suburb', ->(app) { app.suburb&.suburb }],
+      # ['electronic_lodgement', ->(app) { app.electronic_lodgement }],
+      ['engagement_form', ->(app) { app.engagement_form }],
+      ['job_type_administration', ->(app) { app.job_type_administration }],
+      ['quote_accepted_date', ->(app) { app.quote_accepted_date }],
+      ['administration_notes', ->(app) { app.administration_notes }],
+      ['number_of_storeys', ->(app) { app.number_of_storeys }],
+      ['construction_value', ->(app) { app.construction_value }],
+      ['fee_amount', ->(app) { app.fee_amount }],
+      ['building_surveyor', ->(app) { app.building_surveyor }],
+      ['structural_engineer', ->(app) { app.structural_engineer }],
+      ['external_engineer_date', ->(app) { app.external_engineer_date }],
+      ['risk_rating', ->(app) { app.risk_rating }],
+      ['consultancies_review_inspection', ->(app) { app.consultancies_review_inspection }],
+      ['consultancies_report_sent', ->(app) { app.consultancies_report_sent }],
+      ['assessment_commenced', ->(app) { app.assessment_commenced }],
+      ['request_for_information_dates', lambda { |app|
+        app.request_for_informations.map(&:request_for_information_date).join(', ')
+      }],
+      ['consent_issued', ->(app) { app.consent_issued }],
+      ['variation_issued', ->(app) { app.variation_issued }],
+      ['coo_issued', ->(app) { app.coo_issued }],
+      ['engineer_certificate_received', ->(app) { app.engineer_certificate_received }],
+      ['certification_notes', ->(app) { app.certification_notes }],
+      ['invoice_to', ->(app) { app.invoice_to }],
+      ['invoice_email', ->(app) { app.invoice_email }],
+      ['invoice_numbers', ->(app) { app.invoices.map(&:invoice_number).join(', ') }],
+      ['fully_invoiced', ->(app) { app.fully_invoiced }],
+      ['applicant_email', ->(app) { app.applicant_email }],
+      ['created_at', ->(app) { app.created_at }],
+      ['updated_at', ->(app) { app.updated_at }]
+    ]
+
     filename = "JQC_Applications_Export_#{Time.zone.now.strftime('%Y-%m-%d')}.csv"
 
     response.headers.merge!(
@@ -104,17 +155,18 @@ class Application < ApplicationRecord
       'Last-Modified' => Time.current.httpdate
     )
     response.headers.delete('Content-Length')
-    response.stream.write CSV.generate_line(attributes)
+    response.stream.write CSV.generate_line(columns.map(&:first))
 
     applications.find_in_batches(batch_size: 1000) do |batch|
       batch.each do |application|
         response.stream.write CSV.generate_line(
-          attributes.map { |attr| application.send(attr) }
+          columns.map { |_, getter| getter.call(application) }
         )
       end
     end
   ensure
     response.stream.close
   end
-  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 end
+# rubocop:enable Metrics/ClassLength
