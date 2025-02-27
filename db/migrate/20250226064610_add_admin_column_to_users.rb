5c58009942e1a25e208eb43f82fe5721306a486c
Tom Barone <mail@tombarone.net>
2025-02-27 23:45:27 +1030

    Make sure admin user is kept as admin

--- db/migrate/20250226064610_add_admin_column_to_users.rb
+++ db/migrate/20250226064610_add_admin_column_to_users.rb
@@ -3,5 +3,13 @@
 class AddAdminColumnToUsers < ActiveRecord::Migration[8.0]
   def change
     add_column :users, :admin, :boolean, default: false, null: false
+
+    reversible do |dir|
+      dir.up do
+        # Make sure our admin user stays an admin
+        admin_user = User.where(username: 'admin').first
+        admin_user&.update!(admin: true)
+      end
+    end
   end
 end
