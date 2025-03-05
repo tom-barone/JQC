1b98f050bc9b2d86067a2485804fa8ed4d1f0546
Tom Barone <mail@tombarone.net>
2025-03-05 12:09:39 +1030

    Add handler for invalid CSRF token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :prepare_exception_notifier
+  rescue_from ActionController::InvalidAuthenticityToken, with: :invalid_authenticity_token
 
   # Send exception notifications on error
   def prepare_exception_notifier
@@ -16,5 +17,10 @@ class ApplicationController < ActionController::Base
     return if current_user&.admin?
 
     head :forbidden
+  end
+
+  def invalid_authenticity_token
+    sign_out(current_user) if current_user
+    redirect_to new_user_session_url, alert: 'Your session has expired. Please sign in again.'
   end
 end
