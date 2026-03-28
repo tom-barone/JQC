a454a135c8407cd9a5b65c9699b13de6a796bd27
Tom Barone <mail@tombarone.net>
2026-03-28 23:49:43 +1030

    Remove unused tofu var

--- infrastructure/tofu/variables.tf
+++ infrastructure/tofu/variables.tf
@@ -7,11 +7,6 @@ variable "ENVIRONMENT" {
   type = string
 }
 
-variable "BACKUP_PRIMARY_S3_BUCKET_NAME" {
-  type      = string
-  sensitive = true
-}
-
 variable "LINODE_REGION" {
   type      = string
   sensitive = true
