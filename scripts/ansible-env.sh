2c783da34beaf740aae846835e43126bb631a550
Tom Barone <mail@tombarone.net>
2026-04-06 22:42:01 +0930

    Bump ansible-common

--- scripts/ansible-env.sh
+++ scripts/ansible-env.sh
@@ -25,6 +25,6 @@ export SMTP_RELAY_USERNAME=$(echo "$tofu_output_json" | jq -r '.SMTP_RELAY_USERN
 export SMTP_RELAY_PASSWORD=$(echo "$tofu_output_json" | jq -r '.SMTP_RELAY_PASSWORD.value')
 
 # Add the SSH key to our agent first
-echo "$ANSIBLE_SSH_PRIVATE_KEY_B64" | base64 --decode | ssh-add -
+echo "$ANSIBLE_SSH_PRIVATE_KEY_B64" | base64 --decode | ssh-add - 2>/dev/null
 # Removing the server from known_hosts to avoid SSH warnings about changed host keys
 ssh-keygen -R "$JQC_SERVER_IP_ADDRESS"
