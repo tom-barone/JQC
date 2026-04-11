c53af235b7dcc0d662596a35b9544316e5e79ed1
Tom Barone <mail@tombarone.net>
2026-04-11 13:37:21 +0930

    Revert to previous ferrum setup

--- config/initializers/ferrum_pdf.rb
+++ config/initializers/ferrum_pdf.rb
@@ -4,6 +4,3 @@ FerrumPdf.configure do |config|
   config.process_timeout = 15
   config.browser_options = { 'no-sandbox': nil }
 end
-
-# Start the browser now so the first request doesn't pay the startup cost
-FerrumPdf.with_browser { |b| b } # warm up
